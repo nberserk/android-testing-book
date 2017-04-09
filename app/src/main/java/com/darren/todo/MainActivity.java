7272c68aa0b932c6a06189b446e637481bb89bd8
Darren ha <nberserk@gmail.com>
2017-04-09 19:28:48 +0900

    listview added

--- app/src/main/java/com/darren/todo/MainActivity.java
+++ app/src/main/java/com/darren/todo/MainActivity.java
@@ -1,8 +1,10 @@
-package com.darren.todo.todo;
+package com.darren.todo;
 
 import android.os.Bundle;
+import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import com.darren.todo.todo.R;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +48,15 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //
+        String[] myStringArray = {"apple", "orange", "oops", "andrew"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, myStringArray);
+        ListView cl = (ListView) findViewById(R.id.list);
+        cl.setAdapter(adapter);
+        //Log.d("view", cl.toString());
+
     }
 
     @Override
